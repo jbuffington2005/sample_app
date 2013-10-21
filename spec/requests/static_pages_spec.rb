fd5160be77c674d7838e831e50f5c3de07bd1c89
Jason Buffington <jbuffington@madisoncollege.edu>
2013-10-21 13:43:20 -0500

    Finish ex. 3 and 4

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -44,4 +44,19 @@ describe "Static pages" do
 
     it_should_behave_like "all static pages"
   end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help')) # fill in
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact')) # fill in
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up')) # fill in
+    click_link "sample app"
+    expect(page).to have_title(full_title('')) # fill in
+  end
 end
