4f10bcd210d184fdabea53609492fe8645e5ef45
Jason Buffington <jbuffington@madisoncollege.edu>
2013-12-01 22:18:54 -0600

    ch9 ex 3, 5, 6 complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
+  before_action :signed_in_filter, only: [:new, :create]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -59,7 +60,7 @@ class UsersController < ApplicationController
       unless signed_in?
         store_location
         redirect_to signin_url, notice: "Please sign in."
-      end
+      end   
     end
 
     def correct_user
@@ -69,5 +70,11 @@ class UsersController < ApplicationController
 
     def admin_user
       redirect_to(root_url) unless current_user.admin?
+    end
+
+    def signed_in_filter
+      if signed_in?
+        redirect_to(root_url)
+      end
     end
 end
