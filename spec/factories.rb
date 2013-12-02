2157b7beeb1d08b21aa4094925d11df549fe3ae1
Jason Buffington <jbuffington@madisoncollege.edu>
2013-12-01 18:51:40 -0600

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name     "Jason Buffington"
-    email    "stuff@abc.com"
-    password "student"
-    password_confirmation "student"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
